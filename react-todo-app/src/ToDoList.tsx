9ef57f85d760c51912773739c535d40927220801
Sangkook Lee <sangkook.lee@solarsdev.com>
2021-12-15 22:48:57 +0900

    강의 #5.0~#5.8까지 정리

--- react-todo-app/src/ToDoList.tsx
+++ react-todo-app/src/ToDoList.tsx
@@ -1,19 +1,64 @@
-import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import styled from 'styled-components';
+
+interface FormProps {
+  email: string;
+  username: string;
+  password: string;
+  passwordConfirm: string;
+  extraError?: string;
+}
+
+const Error = styled.span`
+  color: red;
+`;
 
 const ToDoList = () => {
-  const [toDo, setToDo] = useState('');
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setToDo(event.currentTarget.value);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onValid = (data: FormProps) => {
+    console.log(data);
   };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(toDo);
-  };
+  console.log(errors);
 
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input type='text' placeholder='Type your to do' value={toDo} onChange={onChange} />
+      <form style={{ display: 'flex', flexDirection: 'column' }} onSubmit={handleSubmit(onValid)}>
+        <input
+          {...register('email', {
+            required: 'Email is required!',
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com/,
+              message: 'Only naver.com email is allowed',
+            },
+          })}
+          placeholder='Enter email'
+        />
+        {errors?.email ? <Error>{errors.email.message}</Error> : null}
+        <input
+          {...register('username', { required: 'Username is required!', minLength: 10 })}
+          placeholder='Enter username'
+        />
+        {errors?.username ? <Error>{errors.username.message}</Error> : null}
+        <input
+          {...register('password', { required: 'Password is required!', minLength: 5 })}
+          placeholder='Enter password'
+        />
+        {errors?.password ? <Error>{errors.password.message}</Error> : null}
+        <input
+          {...register('passwordConfirm', {
+            required: 'Password  Confirm is required!',
+            minLength: {
+              value: 5,
+              message: 'Password is too short',
+            },
+          })}
+          placeholder='Enter password confirm'
+        />
+        {errors?.passwordConfirm ? <Error>{errors.passwordConfirm.message}</Error> : null}
         <input type='submit' value='Add ToDo' />
       </form>
     </div>
